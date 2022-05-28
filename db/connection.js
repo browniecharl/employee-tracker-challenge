1d9fae5881bff1bfcd37b3e6d08e338270dc8fde
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-05-28 08:04:58 -0400

    reworked app.js to finally display all employees and added ability to change employee role

--- db/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'employee_tracker'
-});
-
-module.exports = db;
